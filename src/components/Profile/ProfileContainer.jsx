21eb765b018ad46efdf4b6102b4a492da8890d1c
Ayk_Israelyan <ayk1593@gmail.com>
2021-10-01 19:56:32 +0300

    80-4

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import s from './Profile.module.css';
 import Profile from "./Profile";
 import {connect} from "react-redux";
 import {withRouter} from "react-router-dom";
